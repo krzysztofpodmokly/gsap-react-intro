bb71b6a366519e8f35b477ce8d9e71d4883adce6
Krzysztof <krzysztof.podmokly@gmail.com>
2019-10-20 19:57:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+import { TweenMax, Power3 } from 'gsap';
+
 function App() {
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [state, setState] = useState(false);
+
+  const handleExpand = e => {
+    TweenMax.to(e.target, 0.8, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeOut
+    });
+    setState(true);
+  };
+
+  const handleShrink = e => {
+    TweenMax.to(e.target, 0.8, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeOut
+    });
+    setState(false);
+  };
+
+  useEffect(() => {
+    TweenMax.to(app.current, 0, { css: { visibility: 'visible' } });
+    TweenMax.staggerFrom(
+      [circle.current, circleRed.current, circleBlue.current],
+      0.8,
+      {
+        opacity: 0,
+        x: 40,
+        ease: Power3.easeOut
+      },
+      0.2
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={app} className='App'>
+      <div className='App-header'>
+        <div className='circle-container'>
+          <div
+            ref={circle}
+            className='circle'
+            onClick={state ? e => handleShrink(e) : e => handleExpand(e)}
+          ></div>
+          <div
+            ref={circleRed}
+            className='circle red'
+            onClick={state ? e => handleShrink(e) : e => handleExpand(e)}
+          ></div>
+          <div
+            ref={circleBlue}
+            className='circle blue'
+            onClick={state ? e => handleShrink(e) : e => handleExpand(e)}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
